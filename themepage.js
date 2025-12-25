e39ef977d5ca9ed57a9d93983f0261356aab15e8
Nwabisa <ayandamalawu535@gmail.com>
2025-12-25 23:52:12 +0200

    updated signup page

--- themepage.js
+++ themepage.js
@@ -1,11 +1,27 @@
 let theme_btn = document.getElementById("theme");
 let close_btn = document.getElementById("close");
-let panelcard = document.getElementsByClassName("wholecard")[0];
+let wholecard = document.querySelector(".wholecard");
+let palette_btns = document.querySelectorAll(".theme_selector");
 
+// make card popup on the side
 theme_btn.addEventListener("click", function(){
-    document.body.classList.add("sidepanel_card");
+    wholecard.classList.add("active");
 });
 close_btn.addEventListener("click", function(){
-    document.body.classList.remove("sidepanel_card");
+    wholecard.classList.remove("active");
 })
+
+// make the tick appear on active palette btn
+palette_btns.forEach(current_btn => {
+    
+    current_btn.addEventListener("click", function(){
+        palette_btns.forEach(btn => btn.classList.remove("active"));
+        current_btn.classList.add("active");
+        
+
+    });
+
+});
+    
+
     

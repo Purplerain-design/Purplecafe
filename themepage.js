e4bbfd0c7a2f23498264cfb9ac75d6ba1711f415
Nwabisa <ayandamalawu535@gmail.com>
2026-01-02 21:15:55 +0200

    applied theme on homepage

--- themepage.js
+++ themepage.js
@@ -12,14 +12,33 @@ close_btn2.addEventListener("click", function(){
     wholecard.classList.remove("active");
     document.body.classList.remove("active")
 })
+// save theme even after page refresh
+let savedTheme = localStorage.getItem("theme");
+if(savedTheme){
+    document.documentElement.setAttribute("data-theme", savedTheme);
+
+    palette_btns.forEach(btn => {
+        if(btn.dataset.theme === savedTheme){
+            btn.classList.add("active");
+        }
+
+    });
+}
+
 
 // make the tick appear on active palette btn
+// make picked color palette apply on the entire website
 palette_btns.forEach(current_btn => {
     
     current_btn.addEventListener("click", function(){
+
+        const selectedTheme = current_btn.dataset.theme;
+        document.documentElement.setAttribute("data-theme", selectedTheme);
+
+        localStorage.setItem("theme", selectedTheme);//save theme
+
         palette_btns.forEach(btn => btn.classList.remove("active"));
         current_btn.classList.add("active");
-        
 
     });
 
